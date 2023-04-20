e6cd2564a27dc8940a598fe905e47ddf840577e8
Amanda_L <amandalibbylab@gmail.com>
2023-04-20 15:33:00 -0400

    added async functions

--- js/script.js
+++ js/script.js
@@ -3,7 +3,33 @@ const overview = document.querySelector(".overview");
 const username = "Amanda-Libby";
 
 // go back through the courses to see how to do the javascript needed in this project.
-// do this tomorrow
+
+const gitUserInfo = async function () {
+    const res = await fetch(`https://api.github.com/users/$(username)`);
+    const data = await res.json()
+    console.log(data)
+    displayUserInfo(data)
+};
+gitUserInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div")
+    div.classList.add("user-info")
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${}</p>
+            <p><strong>Bio:</strong> ${}</p>
+            <p><strong>Location:</strong> ${}</p>
+            <p><strong>Number of public repos:</strong> ${}</p>
+        </div> 
+    `;
+    overview.append(div);
+}
+
+// add the data points I need in the div
 
 
 
